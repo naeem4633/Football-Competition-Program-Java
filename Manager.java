3fd20d77af7877fcd43a5589b438517a06094441
naeem4633 <ahmedn3700@gmail.com>
2023-12-25 20:49:03 +0500

    Added a method to display full report but the competitor list if not being read. The program throws an exception.

--- Manager.java
+++ Manager.java
@@ -14,12 +14,42 @@ public class Manager {
     private static void generateReport(String fileName) {
         try {
             CompetitorList competitorList = new CompetitorList("Competitors.csv");
+            CompetitorScoresList competitorScoresList = new CompetitorScoresList("CompetitorScores.csv",
+                    competitorList.getCompetitors());
+
+            // Create an instance of Result
+            Result result = new Result(0, null, null, null);
+
+            // Finding the competitor with the highest overall score
+            Competitor highestScorer = result.getCompetitorWithHighestScore(
+                    competitorScoresList.getCompetitorScoresList(), competitorList.getCompetitors());
+
+            // Calculating summary statistics
+            int maxOverallScore = result.getMaxOverallScore(competitorScoresList.getCompetitorScoresList());
+            int minOverallScore = result.getMinOverallScore(competitorScoresList.getCompetitorScoresList());
+
+            // Generating a frequency report
+            Map<Integer, Integer> frequencyReport = result
+                    .getFrequencyOfScores(competitorScoresList.getCompetitorScoresList());
 
             // Writing the report to a file
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
                 writer.write("Competitors Table:\n");
                 writer.write("-------------------------------------------------------\n");
                 competitorList.displayCompetitors(writer);
+
+                writer.write("\nDetails of the Competitor with the Highest Overall Score: \n"
+                        + highestScorer.getName().getFullName() + "\n");
+
+                writer.write("-------------------------------------------------------\n");
+                // ... display details of the highest scorer as in the previous example ...
+
+                writer.write("\nSummary Statistics:\n");
+                writer.write("-------------------------------------------------------\n");
+                writer.write("Maximum Overall Score: " + maxOverallScore + "\n");
+                writer.write("Minimum Overall Score: " + minOverallScore + "\n");
+
+                writer.write("\nFrequency Report: " + frequencyReport + "\n");
 
                 writer.write("-------------------------------------------------------\n");
 
